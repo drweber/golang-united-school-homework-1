10d71ac5da9dc01b4bdaf5e6d78b71765ad086d8
Nikolai Korytko <drweber@users.noreply.github.com>
2022-02-13 23:59:48 +0100

    print world_map emoji added

--- solution.go
+++ solution.go
@@ -1,5 +1,16 @@
 package solution
 
+import (
+    "fmt"
+
+    "github.com/kyokomi/emoji/v2"
+)
+
 func GetMessage() string {
-	return ""
+	emojiMessage := emoji.Sprint("Hello :world_map:!")
+	return fmt.Println(emojiMessage)
+}
+
+func main() {
+	GetMessage()
 }
