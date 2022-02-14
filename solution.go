299a116ea7c6172778987142fc036c70326e598a
Nikolai Korytko <n_korytko@Mikalai-Comforte.local>
2022-02-14 09:19:02 +0100

    Homework 1

--- solution.go
+++ solution.go
@@ -1,16 +1,9 @@
 package solution
 
 import (
-    "fmt"
-
     "github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
-	emojiMessage := emoji.Sprint("Hello :world_map:!")
-	return fmt.Println(emojiMessage)
-}
-
-func main() {
-	GetMessage()
+	return emoji.Sprint("Hello :world_map:!")
 }
